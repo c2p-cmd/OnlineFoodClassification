968001b18501df51a5ec6a88b732dcf52fed4e33
Sharan Thakur <sharandt19@live.com>
2024-04-20 12:53:21 +0530

    Completes app

--- OnlineFoodOrderingClassifier/OnlineFoodOrderingClassifier/ContentView.swift
+++ OnlineFoodOrderingClassifier/OnlineFoodOrderingClassifier/ContentView.swift
@@ -8,43 +8,133 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var prediction = "0.0"
-    
-    let classifier = try! Classifier()
+    @State private var vm = ViewModel()
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world! \(prediction)")
-            Button("Predict", action: classifier.predict)
+        NavigationStack {
+            Form {
+                Section("Inputs") {
+                    DatePicker(
+                        "Birthdate",
+                        selection: $vm.date,
+                        in: ...Date(),
+                        displayedComponents: .date
+                    )
+                    .datePickerStyle(.automatic)
+                    
+                    Picker("Monthly Income", selection: $vm.monthlyIncome) {
+                        ForEach(MonthlyIncome.allCases, id: \.rawValue) {
+                            Text($0.description)
+                                .tag($0)
+                        }
+                    }
+                    
+                    Stepper {
+                        Text("Family Size **\(vm.familySize)**")
+                    } onIncrement: {
+                        guard vm.familySize < 12 else {
+                            return
+                        }
+                        vm.familySize += 1
+                    } onDecrement: {
+                        guard vm.familySize > 1 else {
+                            return
+                        }
+                        vm.familySize -= 1
+                    }
+                    
+                    Picker("Education", selection: $vm.education) {
+                        ForEach(EducationalQualification.allCases, id: \.rawValue) {
+                            Text($0.description)
+                                .tag($0)
+                        }
+                    }
+                    
+                    Picker("Marital Status", selection: $vm.maritalStatus) {
+                        ForEach(MaritalStatus.allCases, id: \.rawValue) {
+                            Text($0.description)
+                                .tag($0)
+                        }
+                    }
+                    
+                    Picker("Gender", selection: $vm.gender) {
+                        ForEach(Gender.allCases, id: \.rawValue) {
+                            Text($0.description)
+                                .tag($0)
+                        }
+                    }
+                    
+                    Picker("Feedback", selection: $vm.feedback) {
+                        ForEach(Feedback.allCases, id: \.rawValue) {
+                            Text($0.description)
+                                .tag($0)
+                        }
+                    }
+                    
+                    Picker("Occupation", selection: $vm.occupation) {
+                        ForEach(Occupation.allCases, id: \.rawValue) {
+                            Text($0.description)
+                                .tag($0)
+                        }
+                    }
+                }
+                
+                Section("Prediction") {
+                    if let predictionOutput = vm.prediction {
+                        HStack {
+                            Spacer()
+                            Text("*Likely to order? **\(predictionOutput)***")
+                            Spacer()
+                        }
+                    }
+                    if let yesPerc = vm.predictionProbability["Yes"] {
+                        HStack {
+                            Text("***Yes***")
+                            Spacer()
+                            Text("\(yesPerc, format: .percent)")
+                        }
+                    }
+                    if let noPerc = vm.predictionProbability["No"] {
+                        HStack {
+                            Text("***No***")
+                            Spacer()
+                            Text("\(noPerc, format: .percent)")
+                        }
+                    }
+                }
+                
+                Section("Predict") {
+                    Button("Predict", systemImage: "play.circle.fill") {
+                        Task {
+                            do {
+                                try vm.predict()
+                            } catch let error as AppError {
+                                vm.error = error
+                                vm.showError = true
+                            }
+                        }
+                    }
+                }
+            }
+            .fontDesign(.rounded)
+            .alert(isPresented: $vm.showError, error: vm.error) { err in
+                EmptyView()
+            } message: { error in
+                Text(error.description)
+            }
+            .onAppear {
+                do {
+                    try vm.loadModel()
+                } catch let error as AppError {
+                    vm.error = error
+                    vm.showError = true
+                } catch {
+                    print(error)
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Online Food Classification")
         }
-        .padding()
-    }
-}
-
-class Classifier {
-    private let mlModel: OnlineFoodOrderingClassifier
-    
-    init() throws {
-        self.mlModel = try OnlineFoodOrderingClassifier(configuration: .init())
-    }
-    
-    func predict() {
-        let prediction = try? self.mlModel.prediction(
-            Age: 22,
-            Gender: 1,
-            Marital_Status: 2,
-            Occupation: 4,
-            Monthly_Income: 4,
-            Educational_Qualifications: 2,
-            Family_size: 4,
-            Feedback: 0
-        )
-        
-        print(prediction?.Output)
-        print(prediction?.OutputProbability)
     }
 }
 
--- OnlineFoodOrderingClassifier/OnlineFoodOrderingClassifier/ContnetView+ViewModel.swift
+++ OnlineFoodOrderingClassifier/OnlineFoodOrderingClassifier/ContnetView+ViewModel.swift
@@ -5,4 +5,194 @@
 //  Created by Sharan Thakur on 20/04/24.
 //
 
-import Foundation
+import Observation
+import SwiftUI
+
+extension ContentView {
+    @Observable
+    final class ViewModel {
+        // Inputs
+        var date: Date = Date()
+        var monthlyIncome: MonthlyIncome = .noIncome
+        var gender: Gender = .female
+        var maritalStatus: MaritalStatus = .preferNotToSay
+        var education: EducationalQualification = .uneducated
+        var feedback: Feedback = .neutral
+        var occupation: Occupation = .selfEmployeed
+        var familySize: Int64 = 1
+        
+        // Output
+        var predictionProbability: [String : Double] = [:]
+        var prediction: String?
+        
+        var showError = false
+        var error: AppError?
+        
+        private var mlModel: OnlineFoodOrderingClassifier?
+        
+        func loadModel() throws {
+            do {
+                self.mlModel = try OnlineFoodOrderingClassifier(configuration: .init())
+            } catch {
+                print(error)
+                throw AppError.modelLoadError
+            }
+        }
+        
+        func predict() throws {
+            guard let mlModel else {
+                throw AppError.outputError
+            }
+            Task {
+                do {
+                    let age = NSCalendar.current.dateComponents([.year], from: date, to: .now).year!
+                    let prediction = try mlModel.prediction(
+                        Age: Int64(age),
+                        Gender: Int64(gender.rawValue),
+                        Marital_Status: Int64(maritalStatus.rawValue),
+                        Occupation: Int64(occupation.rawValue),
+                        Monthly_Income: Int64(monthlyIncome.rawValue),
+                        Educational_Qualifications: Int64(education.rawValue),
+                        Family_size: familySize,
+                        Feedback: Int64(feedback.rawValue)
+                    )
+                    
+                    self.predictionProbability = [:]
+                    self.prediction = nil
+                    
+                    try await Task.sleep(nanoseconds: 5 * UInt64(pow(10.0, 8.0)))
+                    
+                    self.predictionProbability = prediction.OutputProbability
+                    self.prediction = prediction.Output
+                } catch {
+                    print(error)
+                    throw AppError.outputError
+                }
+            }
+        }
+    }
+}
+
+enum AppError: String, LocalizedError, CustomStringConvertible {
+    case modelLoadError = "Couldn't load Classifier Model"
+    case outputError = "Couldn't run prediction"
+    
+    var description: String {
+        self.rawValue
+    }
+}
+
+// MARK: - Data Models for App
+enum MonthlyIncome: Int, CaseIterable {
+    case noIncome = 1
+    case below10k = 2
+    case moreThan50k = 3
+    case between10To25k = 4
+    case between25kto50k = 5
+    
+    var description: String {
+        switch self {
+        case .noIncome:
+            "No Income"
+        case .below10k:
+            "Below Rs.10,000"
+        case .moreThan50k:
+            "More than Rs.50,000"
+        case .between10To25k:
+            "Between Rs. 10,000 to 25,000"
+        case .between25kto50k:
+            "Between Rs. 25,000 to 50,000"
+        }
+    }
+}
+
+enum Gender: Int, CaseIterable {
+    case male = 1
+    case female = 2
+    
+    var description: String {
+        switch self {
+        case .male:
+            "Male"
+        case .female:
+            "Female"
+        }
+    }
+}
+
+enum MaritalStatus: Int, CaseIterable {
+    case single = 1
+    case married = 2
+    case preferNotToSay
+    
+    var description: String {
+        switch self {
+        case .single:
+            "Single"
+        case .married:
+            "Married"
+        case .preferNotToSay:
+            "Prefer Not To Say"
+        }
+    }
+}
+
+enum EducationalQualification: Int, CaseIterable {
+    case uneducated = 0
+    case school = 1
+    case graduate = 2
+    case postGrad = 3
+    case phd = 4
+    
+    var description: String {
+        switch self {
+        case .uneducated:
+            "Uneducated"
+        case .school:
+            "School"
+        case .graduate:
+            "Graduate"
+        case .postGrad:
+            "Post Graduate"
+        case .phd:
+            "Ph.D"
+        }
+    }
+}
+
+enum Feedback: Int, CaseIterable {
+    case neutral = -1
+    case positive = 1
+    case negative = 0
+    
+    var description: String {
+        switch self {
+        case .neutral:
+            "Neutral"
+        case .positive:
+            "Positive"
+        case .negative:
+            "Negative"
+        }
+    }
+}
+
+enum Occupation: Int, CaseIterable {
+    case student = 1
+    case houseWife = 2
+    case selfEmployeed = 3
+    case employee = 4
+    
+    var description: String {
+        switch self {
+        case .student:
+            "Student"
+        case .houseWife:
+            "House Wife"
+        case .selfEmployeed:
+            "Self Employeed"
+        case .employee:
+            "Employee"
+        }
+    }
+}
